1c2f979c95e3c42693a515399f604000ee377c62
suzukiyo <suzukiyohei.wk@gmail.com>
2015-10-03 18:09:39 +0900

    init animate

--- js/suzukiyo.js
+++ js/suzukiyo.js
@@ -4,16 +4,17 @@
 	$('body').css('background-color', '#286090');
 	$('#title').css('font-size', '70px');
 	$('#subtitle').css('font-size', '30px');
+	$('#menu').css('opacity', '.5');
+	$('#up').css({
+		'position':'fixed',
+		'right':'5%',
+		'bottom':0
+	});
 
 	// This event is menu icon click.
 	$('#menu-btn').click(function(){
 		if ($('.close', this).css('display') === 'block') {
-			$('#menu').css({
-				'display':'none',
-				'position':'absolute'
-			});
-			$('.open', this).css('display', 'block');
-			$('.close', this).css('display', 'none');
+			closemenu();
 		} else {
 			$('#menu').css({
 				'display':'block',
@@ -24,5 +25,29 @@
 		}
 	});
 
+	$('.btn').click(function(){
+		closemenu();
+		scroller($(this).attr('id')+'-area');
+	});
+
+	$('#up').click(function() {
+        $('body,html').animate({scrollTop:0}, 500);
+        return false;
+     });
+
+	function closemenu(){
+		$('#menu').css({
+				'display':'none',
+				'position':'absolute'
+			});
+		$('.open', '#menu-btn').css('display', 'block');
+		$('.close', '#menu-btn').css('display', 'none');
+	}
+
+	function scroller(targetname) {
+		$("html,body").animate({scrollTop:$('#'+targetname).offset().top});
+	}
+
+	
 
 })(jQuery);
