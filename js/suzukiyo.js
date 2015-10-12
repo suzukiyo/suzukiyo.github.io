cf9d5852902c9fb90b49b900bc14b2e4eaa8bcbc
suzukiyo <suzukiyohei.wk@gmail.com>
2015-10-12 16:07:57 +0900

    update design

--- js/suzukiyo.js
+++ js/suzukiyo.js
@@ -1,40 +1,5 @@
 (function($){
 
-		$('body').css('background-color', 'white');
-		// sample css settings
-		$('header').css('margin-bottom', '100px');
-		$('#menu-btn').css({
-			'position':'fixed',
-			'cursor':'pointer',
-			'font-size':'40px'
-		});
-		$('#title').css('font-size', '70px');
-		$('#subtitle').css('font-size', '30px');
-		$('#menu').css({
-			'background-color':'#ccc',
-			'width':'100%',
-			'height':'100%',
-			'display':'none',
-			'bottom':0,
-			'position':'fixed'
-		});
-		$('#up').css({
-			'position':'fixed',
-			'right':'5%',
-			'bottom':0,
-			'cursor':'pointer'
-		});
-		$('.btn').css({
-			'width':'100%',
-			'height':'200px'
-		});
-		$('article').css({
-			'font-size':'20px',
-		});
-		$('img').css({
-			'margin':'30px auto',
-		});
-
 	// This event is menu icon click.
 	$('#menu-btn').click(function(e){
 		if ($('.close-btn', this).css('display') === 'block') {
@@ -47,13 +12,12 @@
 		}
 		e.stopPropagation();
 	});
-	$('#menu').click(function(){
-		e.stopPropagation();
+	$('#menu').click(function(e){
 	});
 
 	$('.btn').click(function(){
 		closemenu();
-		scroller($(this).attr('id')+'-area');
+		scroller('#'+$(this).attr('id')+'-area');
 	});
 
 	$("#up").hide();
@@ -80,30 +44,9 @@
 		$('.close-btn', '#menu-btn').fadeOut('slow');
 	}
 
+	// scroller
 	function scroller(targetname) {
-		$("html,body").animate({scrollTop:$('#'+targetname).offset().top});
+		$("html,body").animate({scrollTop:$(targetname).offset().top});
 	}
-
-	$('#about-area').css({
-		'background-color':'#D4D8D1'
-	});
-	$('#work-area').css({
-		'background-color':'cornflowerblue'
-	});
-	$('#research-area').css({
-		'background-color':'darkseagreen'
-	});
-	$('#improve-area').css({
-		'background-color':'goldenrod'
-	});
-	$('#contact-area').css({
-		'background-color':'hotpink'
-	});
-	$('.com').height('700px');
-
-	$('h1').css({
-		'margin-top':'0px',
-		'margin-bottom':'0px'
-	});
 
 })(jQuery);
